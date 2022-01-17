c2f615fcefb4e75604a91886efe484ef479fcf02
AathifSana <sanaya.aathif961008@gmail.com>
2022-01-17 16:55:36 +0530

    no changes

--- src/main/java/com/CryptoUtils.java
+++ src/main/java/com/CryptoUtils.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 public class CryptoUtils {
 
 
-
     public static final HashMap<String, Double> cryptoPriceMap = new HashMap<>();
 
     public static void initializeCryptoPrices(){
